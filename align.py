b14766d575b8324786f1dd1b20828739f1daf3c9
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-04-29 23:03:29 +0900

    Add split audio in files function implementation

--- align.py
+++ align.py
@@ -1,12 +1,35 @@
 import os, tempfile, config
+from pathlib import Path
+from scipy.io import wavfile
 
 def _read_subs(path):
   # TODO
-  return [(0, 'This is a subtitle example')]
+  return [((0, 0.5), 'This is a subtitle example')]
 
 def _split_audio_in_files(subs, audio_path):
-  # TODO
-  return 'folder'
+  """Saves each sub in a separate audio file and text file"""
+
+  # Arbitrary name
+  folder_name = 'subs'
+  Path(folder_name).mkdir(parents=True, exist_ok=True)
+
+  rate, data = wavfile.read(audio_path)
+
+  for i, sub in enumerate(subs):
+    # Audio processing
+    # Cuts the audio file at the proper frames
+    # Saves the new audio to an individual file
+    start_frame = int(sub[0][0]*rate)
+    end_frame = int(sub[0][1]*rate)
+
+    sub_audio_data = data[start_frame:end_frame]
+    wavfile.write(os.path.join(folder_name, str(i)+".wav"), rate, sub_audio_data)
+
+    # Writes the subtitle in a .lab file
+    with open(os.path.join(folder_name, str(i)+".lab"), "w") as subfile:
+      subfile.write(sub[1])
+
+  return folder_name
 
 def align_phonems(audio_path, subs_path):
   subs = _read_subs(subs_path)
