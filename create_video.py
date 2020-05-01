ae825caa001d3a81f703e6d2a9163564ffad097c
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-05-01 20:03:31 +0900

    Add audio stash system

--- create_video.py
+++ create_video.py
@@ -63,28 +63,54 @@ if __name__ == "__main__":
     total_text = ""
     sentence = get_sentence(None)
     links = argv[1:]
+
+    timestamps_buffer = []
     while sentence != '':
       timestamps = []
 
       edit = False
+      store = False
       valid = False
+      load_audio_index = None
       while not valid:
         if edit:
           sentence = get_sentence(total_text)
 
-        bad_sentence = True
-        while bad_sentence:
-          try:
-            timestamps = main(sentence, links, skip=skip)
-            bad_sentence = False
-          except KeyError as e:
-            print(e, 'not recognized')
-            sentence = get_sentence(total_text)
+        # Stores previous audio in buffer
+        if store:
+          timestamps_buffer.append(timestamps)
+          store = False
+
+        if load_audio_index is not None:
+          timestamps = timestamps_buffer[load_audio_index]
+          load_audio_index = None
+        else:
+          bad_sentence = True
+          while bad_sentence:
+            try:
+              timestamps = main(sentence, links, skip=skip)
+              bad_sentence = False
+            except KeyError as e:
+              print(e, 'not recognized')
+              sentence = get_sentence(total_text)
+
+        _concat_wav(timestamps)
 
         os.system(command)
-        line = input("Enter 'y' to validate, 'e' to edit the sentence, otherwise just press enter: ")
+
+        line = input("Enter 'y' to validate, 'e' to edit the sentence, 's' to store this audio in the buffer, 'l' + index for loading previously stored audio, otherwise just press enter: ")
         valid = line == 'y'
         edit = line == 'e'
+        store = line == 's'
+
+        if line.startswith("l "):
+            index = line.split(" ")[1]
+            if index.isdigit():
+              index = int(index)
+              if -1 < index < len(timestamps_buffer):
+                load_audio_index = index
+                print(load_audio_index)
+
         skip = True
         os.system('cls' if os.name == 'nt' else 'clear')
 
