e91f421662cd488d467fe83482c7bb60dbfcb9b2
Louis MOUHAT <louis.mouhat@gmail.com>
2020-05-03 03:26:20 +0900

    workaround youtube-dl issue #5710

--- video_creator/download.py
+++ video_creator/download.py
@@ -1,11 +1,25 @@
-import youtube_dl, os
+import youtube_dl, os, re
+
+# workaround for youtube-dl issue #5710
+class Logger(object):
+  final_path = None
+  def debug(self, msg):
+    match = re.search(r'^\s*\[download\]\s(.*?)\shas.*$', msg)
+    if match:
+      self.final_path = match.group(1)
+  def warning(self, msg):
+    pass
+  def error(self, msg):
+    print(msg)
 
 def dl_video(url):
+  log = Logger()
   ydl_opts = {
     'outtmpl': 'downloads/%(title)s.%(ext)s',
+    'logger': log,
   }
   with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     filename = ydl.prepare_filename(ydl.extract_info(url))
     if not os.path.exists(filename):
       ydl.download([url])
-    return filename
+    return log.final_path or filename
