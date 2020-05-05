a45180e104c12ef3f376c0ff174d138c189771a4
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-05-06 01:11:14 +0900

    Add consonant-vowel dict related functions

--- logic/text_parser.py
+++ logic/text_parser.py
@@ -1,12 +1,12 @@
+import functools
 import re
-from functools import lru_cache
 
 from num2words import num2words
 
 import config
 
 
-@lru_cache(maxsize=None)
+@functools.lru_cache(maxsize=None)
 def get_dict():
     """Retrieves the dictionary and parses it to a python dict"""
 
@@ -17,6 +17,37 @@ def get_dict():
         phonem_dict = dict(x.rstrip().split(None, 1) for x in f)
 
     return phonem_dict
+
+
+@functools.lru_cache(maxsize=None)
+def get_consonant_vowel_dict():
+    """
+    Retrieves the consonant-vowel dictionary and parses it to a reverse python dict
+    Python dict structure: phonem -> phonem_category
+
+    Three categories available: CONSONANT, VOWEL, SPACE
+    """
+
+    dict_path = config.get_property("dict_consonant_vowel_path")
+
+    with open(dict_path) as f:
+        consonant_vowel_dict = dict(
+            [
+                [elem, line.split()[0]]
+                for line in f
+                for elem in line.split()[1:]
+            ]
+        )
+
+    return consonant_vowel_dict
+
+
+@functools.lru_cache(maxsize=None)
+def get_all_phonems():
+    """Retrieves a list of all phonems relying on the consonant-vowel dictionary"""
+
+    with open(config.get_property("dict_consonant_vowel_path")) as f:
+        return [phonem for line in f for phonem in line.split()[1:]]
 
 
 def transform_numbers(text):
