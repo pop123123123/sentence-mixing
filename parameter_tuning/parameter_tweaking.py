46a4742fbc93f563b0da9881ea5f519abc51f3fd
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-05-12 22:47:39 +0900

    Add parameter tuning stub

---
+++ parameter_tuning/parameter_tweaking.py
@@ -0,0 +1,22 @@
+import logic.parameters as params
+
+minimal_phonem_range = [0, 0.03, 0.1]
+maximal_consonant_length_range = [0.10, 0.25, 0.5]
+maximal_vowel_length_range = [0.10, 0.25, 0.5]
+silence_power_range = [1, 2, 4]
+start_modif_range = [0.8, 1.0, 1.2]
+rate_poer_range = [0.9, 1.1, 1.3]
+
+
+maximal_minimal_phonem_length_malus_range = [500, 1000, 2000]
+maximal_maximal_consonant_length_malus_range = [500, 1000, 2000]
+maximal_maximal_vowel_length_malus_range = [500, 1000, 2000]
+rating_length_same_phonem_range = [80, 150, 250]
+rating_length_same_word_range = [100, 170, 300]
+score_same_transcription_range = [0, 200, 400]
+score_silence_amplitude_range = [0, 200, 400]
+score_duration_rang = [0, 200, 400]
+rating_spectral_similarity_range = [0, 50, 150]
+rating_amplitude_difference_range = [-500, -250, 0]
+score_same_audio_word_range = [80, 200, 500]
+random_span_range = [0, 50, 100]
