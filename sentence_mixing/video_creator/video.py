ba6fc8e29bb1cdf1ed988551cb7ce98ee5545b26
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-05-30 22:29:53 +0900

    Add logger entry for create video

--- sentence_mixing/video_creator/video.py
+++ sentence_mixing/video_creator/video.py
@@ -3,6 +3,6 @@ import os
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
 
-def create_video_file(total_phonems, path):
+def create_video_file(total_phonems, path, logger="bar"):
     clips = [phonem.get_video_clip() for phonem in total_phonems]
-    concatenate_videoclips(clips).write_videofile(path)
+    concatenate_videoclips(clips).write_videofile(path, logger=logger)
