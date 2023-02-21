fccaf9d14980d3775d6ffd5e430d2ae2ff2385b7
Nicolas BUSSER <nicolas.busser67@gmail.com>
2023-02-21 23:40:07 +0100

    set n pronunciations to 1 for g2p

--- sentence_mixing/logic/text_parser.py
+++ sentence_mixing/logic/text_parser.py
@@ -166,7 +166,7 @@ def infer_phonems(token):
         exe_path = config.get_property("g2p_exe")
         model_path = config.get_property("g2p_model")
         command = (
-            f'{exe_path} "{path}" "{model_path}" "{out_path}"'
+            f'{exe_path} "{path}" "{model_path}" "{out_path}" --num_pronunciations 1'
         )
         os.system(command)
         dictionnary = parse_dict(out_path)
